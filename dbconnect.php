7b0da7578e7151e7e2b1ba110b285d8534a57b2e
lukynmatuska <lukynmatuska@gmail.com>
2018-03-14 21:04:02 +0100

    dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,11 +1,8 @@
 <?php 
-$servername = "localhost";
-$username = "carodky";
-$password= "VKGSXcKmO7vpZL5O";
-$dbname = "carodky2018";
-$vzkaz = "Ahoj svete!"; 
 
 $conn = new mysqli($servername,$username,$password,$dbname); 
 if($conn->connect_error){
     die($conn->connect_error); }
+$conn -> set_charset("UTF8") or die("Spatne kodovani!");
+
 ?>
