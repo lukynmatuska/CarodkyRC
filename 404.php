87780ecdc142b6eae68cee0440a780cf4575df48
Lukáš Matuška <lukynmatuska@gmail.com>
2019-02-27 08:16:25 +0100

    Reformat error page

--- 404.php
+++ 404.php
@@ -1,13 +1,10 @@
 <?php  include realpath($_SERVER['DOCUMENT_ROOT']).'/htmlParts/header.php'; ?>
         <title>404 Nenalezeno - Čarodky Rudice</title>
-        <meta property="og:title" content="404 Nenalezeno - Čarodky Rudice" />
         <meta property="og:type" content="Error page" />
-        <meta property="og:url" content="https://carodky.buchticka.eu/chyba404.php" />
-        <meta property="og:image" content="https://carodky.buchticka.eu/background.jpg" />
+        <meta property="og:image" content="/background.jpg" />
         <meta property="og:description" content="Error page" />
-        <meta property="fb:app_id" content="" />
     </head>
-    <body style="background-color: transparent; font-family: Trebuchet MS">
+    <body style="background-color: transparent; ">
         <div style="width:100%; " class="mui-container">
             <div class="mui-panel" style="text-align:center">
                 <h1 style="text-align:center">404 Nenalezeno</h1>
