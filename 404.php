b076afe0b10788b352265cc75670ec7fa9810393
Lukáš Matuška <lukynmatuska@gmail.com>
2019-02-27 08:06:17 +0100

    Reformat error pages

--- 404.php
+++ 404.php
@@ -1,32 +1,19 @@
 <?php  include realpath($_SERVER['DOCUMENT_ROOT']).'/htmlParts/header.php'; ?>
-    <title>404 Nenalezeno - Čarodky Rudice</title>
-    <meta property="og:title" content="404 Nenalezeno - Čarodky Rudice" />
-    <meta property="og:type" content="Error page" />
-    <meta property="og:url" content="https://carodky.buchticka.eu/chyba404.php" />
-    <meta property="og:image" content="https://carodky.buchticka.eu/background.jpg" />
-    <meta property="og:description" content="Error page" />
-    <meta property="fb:app_id" content="" />
-</head>
-<body style="background-color: transparent; font-family: Trebuchet MS">
-    <div style="width:100%; " class="mui-container">
-      <div class="mui-panel" >
-      <div style="text-align:center">
-  <h1 style="text-align:center">404 Nenalezeno</h1>
-  <a href="./">                      <button style="margin-left:auto;margin-right:auto;margin-top:auto;margin-bottom:auto;" class="mui-btn mui-btn--primary mui-btn--raised">Přání  </button></a>
-  <a href="./zpetnavazba">           <button style="margin-left:auto;margin-right:auto;margin-top:auto;margin-bottom:auto;" class="mui-btn mui-btn--primary mui-btn--raised">Zpětná vazba  </button></a>
-  
-  
-
-
-
-<div class="paticka" style="text-alig: center;">
-   <hr ><p style="text-align: center; font-size: 75%; border=0%; padding=0%"> Copyright &copy; 2018, <a href="http://buchticka.eu">Buchticka.eu</a> Team <!--<a href="mailto:posta@buchticka.eu" class="blind">posta@buchticka.eu</a>-->
-   </p>
-   </div>
-   
-   </div>
-</div>
-</div>
-
-</body>
+        <title>404 Nenalezeno - Čarodky Rudice</title>
+        <meta property="og:title" content="404 Nenalezeno - Čarodky Rudice" />
+        <meta property="og:type" content="Error page" />
+        <meta property="og:url" content="https://carodky.buchticka.eu/chyba404.php" />
+        <meta property="og:image" content="https://carodky.buchticka.eu/background.jpg" />
+        <meta property="og:description" content="Error page" />
+        <meta property="fb:app_id" content="" />
+    </head>
+    <body style="background-color: transparent; font-family: Trebuchet MS">
+        <div style="width:100%; " class="mui-container">
+            <div class="mui-panel" style="text-align:center">
+                <h1 style="text-align:center">404 Nenalezeno</h1>
+                <?php  include realpath($_SERVER['DOCUMENT_ROOT']).'/htmlParts/menu.php'; ?>
+                <?php  include realpath($_SERVER['DOCUMENT_ROOT']).'/htmlParts/footer.php'; ?>
+            </div>
+        </div>
+    </body>
 </html>
